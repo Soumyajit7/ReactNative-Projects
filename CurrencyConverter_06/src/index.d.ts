2727b446b771765377f4c1ad1a1ed54ab258bf90
Er. Soumyajit Pan <soumyajitmahi7@gmail.com>
2023-05-24 21:15:45 +0530

    Upload Project 6 : Currency Converter

---
+++ CurrencyConverter_06/src/index.d.ts
@@ -0,0 +1,13 @@
+// interface Currency {
+//     name: string;
+//     value: Number;
+//     flag: string;
+//     symbol: string;
+// }
+
+type Currency = {
+    name: string;
+    symbol: string;
+    value: number;
+    flag: string;
+};
